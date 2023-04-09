135b551b91f8bfd688479fee76bb1999b7b58013
zulbil <alexkhang25@yahoo.fr>
2023-04-08 23:08:04 -0500

    update

--- serverless.ts
+++ serverless.ts
@@ -18,7 +18,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-      UPLOADER_S3_BUCKET: '${env:UPLOADER_S3_BUCKET}',
+      UPLOADER_S3_BUCKET: 'uploader-s3-bucket',
     }
   },
   // import the function via paths
--- src/functions/file-upload/index.ts
+++ src/functions/file-upload/index.ts
@@ -1,11 +1,12 @@
 import { handlerPath } from '@libs/handler-resolver';
 
+const bucketName = process.env.UPLOADER_S3_BUCKET || 'uploader-s3-bucket';
 export default {
   handler: `${handlerPath(__dirname)}/handler.mediaProcessor`,
   events: [
     {
       s3: {
-        bucket: 'media-s3-bucket',
+        bucket: bucketName,
         event: 's3:ObjectCreated:*',
         rules: [
           {
