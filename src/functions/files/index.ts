ffd257666d3f608147cb92ee570da084cf46aa8d
zulbil <alexkhang25@yahoo.fr>
2023-05-18 09:24:06 -0500

    refactor code structure

--- src/functions/files/index.ts
+++ src/functions/files/index.ts
@@ -13,7 +13,8 @@ export const mediaProcessor =  {
         event: 's3:ObjectCreated:*',
         rules: [
           {
-            prefix: 'input/'
+            prefix: 'input/',
+            suffix: 'jpg|jpeg|png'
           }
         ]
       }
